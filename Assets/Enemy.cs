7f455b9272a5dc687c771b764dd20542002d2cee
5urviliuk5 <145654904+5urviliuk5@users.noreply.github.com>
2024-01-04 18:46:15 +0200

    animator (slenderman figure)

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -1,33 +1,48 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
-    public Transform target;
-    public float speed;
-    Rigidbody rb;
-    NavMeshAgent agent;
     public float viewDistance;
     public float wanderDistance;
+    public float speed;
+    public Transform target;
+    public Animator animator;
+
+    Rigidbody rb;
+    NavMeshAgent agent;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         agent = GetComponent<NavMeshAgent>();
-        agent.speed = speed;
     }
 
     void Update()
     {
-        var distance = Vector3.Distance(transform.position, target.position);
+        var currentSpeed = agent.velocity.magnitude;
+        if (currentSpeed == 0)
+        {
+            animator.Play("Idle");
+        }
+        else if (currentSpeed < 3)
+        {
+            animator.Play("Walk");
+        }
+        else
+        {
+            animator.Play("Run");
+        }
 
+        agent.speed = speed;
+
+
+        var distance = Vector3.Distance(transform.position, target.position);
         if (distance < viewDistance)
         {
             agent.destination = target.position;
         }
-        else 
+        else
         {
             if (agent.velocity == Vector3.zero)
             {
@@ -38,7 +53,7 @@ public class Enemy : MonoBehaviour
 
         if (distance < 1f)
         {
-            // jumpscare
+            // JUMPSCARE
         }
     }
 }
